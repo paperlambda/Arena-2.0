7d5b108f87d9d0e1440684ad6164ad2179de6972
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-02 04:24:27 +0000

    Deploy siesgstarena/Arena-2.0 to github.com/siesgstarena/Arena-2.0.git:gh-pages

---
+++ precache-manifest.1e69337a6784a4fb520ad6b2b5a24d24.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff0763fda470ad831461",
+    "url": "/Arena-2.0/static/css/main.91194617.chunk.css"
+  },
+  {
+    "revision": "ff0763fda470ad831461",
+    "url": "/Arena-2.0/static/js/main.ff0763fd.chunk.js"
+  },
+  {
+    "revision": "fe7f32addf2b05da9468",
+    "url": "/Arena-2.0/static/css/1.790a5583.chunk.css"
+  },
+  {
+    "revision": "fe7f32addf2b05da9468",
+    "url": "/Arena-2.0/static/js/1.fe7f32ad.chunk.js"
+  },
+  {
+    "revision": "8f81d8257f617a514fe3",
+    "url": "/Arena-2.0/static/js/runtime~main.8f81d825.js"
+  },
+  {
+    "revision": "1acf923c09564bf1986e1859217568c5",
+    "url": "/Arena-2.0/static/media/mbri-add-submenu.1acf923c.svg"
+  },
+  {
+    "revision": "4f4734e2ff821fe417a5e6018bee269c",
+    "url": "/Arena-2.0/static/media/tshirt.4f4734e2.jpg"
+  },
+  {
+    "revision": "ab2ee490f0133cfb10a1cd44058d4af3",
+    "url": "/Arena-2.0/static/media/bag.ab2ee490.jpg"
+  },
+  {
+    "revision": "a7e6457e48f6028f4dfd7857d50661ff",
+    "url": "/Arena-2.0/static/media/coders.a7e6457e.jpg"
+  },
+  {
+    "revision": "57e5cdd1f473dc82df474e251e5fac37",
+    "url": "/Arena-2.0/static/media/coders1.57e5cdd1.jpg"
+  },
+  {
+    "revision": "34513e4af7a416c7ce32b3bb7fb322d7",
+    "url": "/Arena-2.0/index.html"
+  }
+];
